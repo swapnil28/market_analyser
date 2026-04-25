132e04a00a96a37a9224cee2cd8d59604e0bf7f8
Swapnil <swapnil.nikam@gmail.com>
2026-04-25 22:33:41 +0530

    Fix PORT environment variable for Google Cloud

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,6 @@
 import sys
 import os
 
-# Add backend folder to Python path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
 
 from app import create_app, db
@@ -10,5 +9,5 @@ from app import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
