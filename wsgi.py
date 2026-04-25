620c899dcd83c3732e277c7b82dff79e6abedb4a
Swapnil <swapnil.nikam@gmail.com>
2026-04-25 22:38:19 +0530

    Use wsgi app object for gunicorn

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,3 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
 from app import create_app, db
 
 app = create_app()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
